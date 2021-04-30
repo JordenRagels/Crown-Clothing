72ab5eba4fe674fee237886681e0e4407617fc08
Jorden <jordenragels@Jordens-MacBook-Air.local>
2021-04-30 13:14:16 -0600

    directory component

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component'; 
 
@@ -45,7 +44,7 @@ class Directory extends React.Component {
         }
         render() {
             return (
-                <div className='directory-menu'>
+              <div className='directory-menu'>
                 {
                     this.state.sections.map(({ title, imageUrl, id }) => (
                         <MenuItem key={id} title={title} /> 
