a3d069d27c65bd97120be8eae956787632d37d58
Jorden Ragelse <jordenragels3@gmail.comm>
2021-07-19 14:40:22 -0600

    updated file tree

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component.jsx';
+
+
+function App() {
+  return (
+    <div>
+     <Switch>
+       <Route exact path='/' component={HomePage} /> 
+       <Route path='/shop' component={ShopPage} /> 
+     </Switch>
+    </div>
+  );
+}
+
+export default App;
